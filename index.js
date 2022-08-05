326605ccb2b43006d770dc56ae0efcab547aa05d
Maria Rios <kanelapr@gmail.com>
2022-08-05 15:12:15 -0400

    404 view

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', (req,res) => {
 
 //wildcard
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)
